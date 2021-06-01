b8f5ba1907c353170a7c8ca9b0b887ca53a3bf73
lou.lawrence@kitware.com <lou.lawrence@kitware.com>
2021-06-01 15:14:33 -0400

    test no dependencies on tests passing

--- share/spack/qa/windows_test_setup.ps1
+++ share/spack/qa/windows_test_setup.ps1
@@ -1,5 +1,6 @@
+Write-Host "CURRENT LOCATION: $PSScriptRoot"
 Set-Location ../
 mkdir scripts
 Copy-Item "spack\lib\spack\spack\cmd\installer\scripts\*" "scripts"
 Copy-Item "spack\lib\spack\spack\cmd\installer\spack_cmd.bat" "."
-.\spack_cmd.bat
+Invoke-Expression -Command .\spack_cmd.bat

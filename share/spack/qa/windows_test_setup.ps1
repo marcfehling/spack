aaa0e3952377976567bf6c03f71e4bed3ddcaf6c
lou.lawrence@kitware.com <lou.lawrence@kitware.com>
2021-06-08 10:12:00 -0400

    test full unit-test run

--- share/spack/qa/windows_test_setup.ps1
+++ share/spack/qa/windows_test_setup.ps1
@@ -1,14 +1,8 @@
-Write-Host "CURRENT LOCATION: $pwd"
-#$env:SPACK_ROOT="$PWD"
 Set-Location ../
-Write-Host "CURRENT LOCATION: $pwd"
-Write-Host "SPACK ROOT: $env:SPACK_ROOT"
 mkdir scripts
 Copy-Item "spack\lib\spack\spack\cmd\installer\scripts\*" "scripts"
 Copy-Item "spack\lib\spack\spack\cmd\installer\spack_cmd.bat" "."
 $env:python_pf_ver="C:\hostedtoolcache\windows\Python\3.9.5\x64\python.exe"
-Write-Host "python: $env:python_pf_ver"
-#Invoke-Expression -Command .\spack_cmd.bat
 cmd /c "`".\spack_cmd.bat`" print " |
 foreach {
     if ($_ -match "=") {
@@ -16,5 +10,3 @@ foreach {
         [Environment]::SetEnvironmentVariable($v[0], $v[1])
     }
 }
-Write-Host "TEST UTIL"
-spack 
